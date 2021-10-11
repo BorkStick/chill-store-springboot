f6b59c2c0f58b5a604f00012f34f020532adf834
Tylor Wurz <4ciiDicBa55@gmail.com>
2021-10-11 00:20:05 -0500

    making good progress

---
+++ src/main/java/tech/tylor/ecommerce/configuration/SecurityConfiguration.java
@@ -0,0 +1,50 @@
+package tech.tylor.ecommerce.configuration;
+
+import tech.tylor.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AndRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/cart").authenticated()
+                .antMatchers("/h2-console/**").permitAll()
+                .and().formLogin()
+                .loginPage("/signin")
+                .loginProcessingUrl("/login")
+                .and().logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/signout"))
+                .logoutSuccessUrl("/");
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+    }
+}
---
+++ src/main/java/tech/tylor/ecommerce/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package tech.tylor.ecommerce.repository;
+
+import tech.tylor.ecommerce.model.User;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findByUsername(String username);
+
+}
